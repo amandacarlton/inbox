0a84b20f257f9294c4dbfbb4fa9d3aae6068cdae
Amanda Carlton <acarlton27@gmail.com>
2015-09-10 07:54:01 -0600

    read multiselect

--- js/controller.js
+++ js/controller.js
@@ -4,30 +4,90 @@ app.controller("InboxController", ['$scope', 'InboxService', '$location', '$loca
     '$sessionStorage', function ($scope, InboxService, $location, $localStorage, $sessionStorage ) {
 $scope.name = "amanda";
 $scope.clicked = false;
+$scope.staricon = false;
 $scope.$storage = $localStorage;
 
-$scope.persist = function () {
-  // $scope.item = InboxService.storagearray();
-  $scope.$storage.test = InboxService.storagearray();
-  console.log($scope.$storage.test);
-};
+
+// $scope.persist = function () {
+//   // $scope.item = InboxService.storagearray();
+//   $scope.$storage.test = InboxService.storagearray();
+//   console.log($scope.$storage.test);
+// };
 
 // $scope.anyselected = false;
 $scope.messages = InboxService.messages();
 
+
+$scope.star = function () {
+  this.staricon = !this.staricon;
+};
+
+$scope.singleStar = function () {
+  InboxService.singleStar(this.message);
+};
+
 $scope.icon = function () {
-  this.clicked=!this.clicked;
-  InboxService.iconButton();
+  $scope.clicked=true;
+  // InboxService.iconButton();
 };
-
+//
 $scope.noicon = function () {
-  this.clicked=!this.clicked;
-  InboxService.noIconButton();
+  $scope.clicked=false;
+  // InboxService.noIconButton();
 };
 
-$scope.singleSelect = function () {
-  $scope.anyselected = InboxService.singleSelect(this.message);
+$scope.anySelect = function () {
+  var test = [];
+  for (var i = 0; i < $scope.$storage.activated.length; i++) {
+    if($scope.$storage.activated[i]===true){
+      test.push(true);
+    }
+  }
+  if(test.length>0){
+    $scope.anyselected = true;
+  }else{
+    $scope.anyselected = false;
+  }
 };
 
+$scope.readcount = InboxService.readcount();
+console.log($scope.readcount);
 
+$scope.clickedlist = function () {
+  $scope.$storage.activated = InboxService.storagearray();
+};
+
+$scope.nonclickedlist = function () {
+  $scope.$storage.activated = InboxService.nonstoragearray();
+};
+
+$scope.submitindex = function (num) {
+  var value = $scope.$storage.activated[num];
+  if (value === true){
+    $scope.$storage.activated[num] = false;
+  }else{
+    $scope.$storage.activated[num] = true;
+  } console.log($scope.$storage.activated);
+};
+
+$scope.read = function () {
+  for (var i = 0; i < $scope.messages.length; i++) {
+    if($scope.$storage.activated[i]===true){
+      $scope.messages[i].read = true;
+    }
+  }
+$scope.readcount = InboxService.readcount();
+console.log($scope.readcount);
+  console.log($scope.messages);
+};
+
+$scope.unread = function () {
+  console.log("working");
+  for (var i = 0; i < $scope.messages.length; i++) {
+      if($scope.$storage.activated[i]===true){
+          $scope.messages[i].read = false;
+  }
+} $scope.readcount = InboxService.readcount();
+console.log($scope.messages);
+};
 }]);
--- js/services.js
+++ js/services.js
@@ -1,69 +1,100 @@
 app.factory('InboxService', function () {
 var anyselected = false;
+var selected = false;
+var clicked = false;
 var messages = [
   {
     subject: "HEY",
-    selected:false
+    starred: false,
+    read: false
 
   },
   {
     subject: "Do you see this",
-    selected:false
+    starred: false,
+    read: false
   },
   {
     subject: "How are you?",
-    selected:false
+    starred: false,
+    read: false
   },
   {
     subject: "Does this look like gmail",
-    selected:false
+    starred: false,
+    read: false
   }
 ];
 
 var services = {
   messages: function () {
+    console.log(messages);
     return messages;
   },
 
-  iconButton: function () {
-    for (var i = 0; i < messages.length; i++) {
-      messages[i].selected = true;
-    }
-  },
-  noIconButton: function () {
-    for (var i = 0; i < messages.length; i++) {
-      messages[i].selected = false;
-    }
+  // iconButton: function () {
+  //   for (var i = 0; i < messages.length; i++) {
+  //     messages[i].selected = true;
+  //   }
+  // },
+  // noIconButton: function () {
+  //   for (var i = 0; i < messages.length; i++) {
+  //     messages[i].selected = false;
+  //   }
+  // },
+
+  singleStar: function (message) {
+    message.starred = !message.starred;
   },
 
-  singleSelect: function (message) {
-    message.selected= !message.selected;
-    var selectarr=[];
-      for (var i = 0; i < messages.length; i++) {
-        var place = selectarr.indexOf(messages[i]);
-        if(messages[i].selected === true){
-          selectarr.push(messages[i]);
-        }else{
-          if(selectarr.indexOf(messages[i])>-1){
-            selectarr.splice(place,1);
-          }
-        }
-      } if(selectarr.length >0){
-        anyselected = true;
-      }else{
-        anyselected = false;
-      } 
-      return anyselected;
-    },
+  // singleSelect: function (message) {
+  //   message.selected= !message.selected;
+  //
+  //   var selectarr=[];
+  //     for (var i = 0; i < messages.length; i++) {
+  //       var place = selectarr.indexOf(messages[i]);
+  //       if(messages[i].selected === true){
+  //         selectarr.push(messages[i]);
+  //       }else{
+  //         if(selectarr.indexOf(messages[i])>-1){
+  //           selectarr.splice(place,1);
+  //         }
+  //       }
+  //     } if(selectarr.length >0){
+  //       anyselected = true;
+  //     }else{
+  //       anyselected = false;
+  //     }
+  //     return anyselected;
+  //   },
 
     storagearray: function () {
       var store = [];
       for (var i = 0; i < messages.length; i++) {
-        store.push(messages[i].selected);
+        store.push(true);
       }
+      console.log(store);
       return store;
-    }
 
+    },
+
+    nonstoragearray: function () {
+      var store = [];
+      for (var i = 0; i < messages.length; i++) {
+        store.push(false);
+      }
+      console.log(store);
+      return store;
+    },
+
+    readcount: function () {
+      var readarray = [];
+      for (var i = 0; i < messages.length; i++) {
+        if(messages[i].read === false){
+          readarray.push(messages[i]);
+        }
+      } return readarray.length;
+    }
 
 
 
