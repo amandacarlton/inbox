9a4f9f4fa7ec0a43b29c4b81044f3752d36520d9
Amanda Carlton <acarlton27@gmail.com>
2015-09-12 21:39:32 -0600

    inbox

--- js/controller.js
+++ js/controller.js
@@ -1,137 +1,149 @@
 
 
 app.controller("InboxController", ['$scope', 'InboxService', '$location', '$localStorage',
-    '$sessionStorage', 'ModalService', function ($scope, InboxService, $location, $localStorage, $sessionStorage, ModalService ) {
+'$sessionStorage', 'ModalService', function ($scope, InboxService, $location, $localStorage, $sessionStorage, ModalService ) {
 
-InboxService.messages().then(function (messages) {
+  InboxService.messages().then(function (messages) {
 
-$scope.messages = messages;
-$scope.readcount();
-$scope.anySelect();
-console.log($scope.messages);
-});
+    $scope.messages = messages;
+    $scope.readcount();
+    $scope.anySelect();
+    console.log($scope.messages);
+  });
 
-$scope.name = "amanda";
-$scope.clicked = false;
-$scope.staricon = false;
-$scope.$storage = $localStorage;
-
-
-// $scope.persist = function () {
-//   // $scope.item = InboxService.storagearray();
-//   $scope.$storage.test = InboxService.storagearray();
-//   console.log($scope.$storage.test);
-// };
-
-// $scope.anyselected = false;
-// $scope.messages = InboxService.messages();
+  $scope.name = "amanda";
+  $scope.clicked = false;
+  $scope.staricon = false;
+  $scope.$storage = $localStorage;
 
 
-$scope.star = function () {
-  this.staricon = !this.staricon;
-};
+  // $scope.persist = function () {
+  //   // $scope.item = InboxService.storagearray();
+  //   $scope.$storage.test = InboxService.storagearray();
+  //   console.log($scope.$storage.test);
+  // };
 
-$scope.singleStar = function () {
-  InboxService.singleStar(this.message);
-};
-
-$scope.icon = function () {
-  $scope.clicked=true;
-  // InboxService.iconButton();
-};
-//
-$scope.noicon = function () {
-  $scope.clicked=false;
-  // InboxService.noIconButton();
-};
-
-$scope.anySelect = function () {
-  var test = [];
-  for (var i = 0; i < $scope.$storage.activated.length; i++) {
-    if($scope.$storage.activated[i]===true){
-      test.push(true);
-    }
-  }
-  if(test.length>0){
-    $scope.anyselected = true;
-  }else{
-    $scope.anyselected = false;
-  }
-};
-
-$scope.readcount = function () {
-  var unreadarray = [];
-  for (var i = 0; i < $scope.messages.length; i++) {
-    if($scope.messages[i].read === false){
-      unreadarray.push($scope.messages[i]);
-    }
-  }
-  console.log(unreadarray);
-  $scope.unreadcount= unreadarray.length;
-};
+  // $scope.anyselected = false;
+  // $scope.messages = InboxService.messages();
 
 
+  $scope.star = function () {
+    this.staricon = !this.staricon;
+  };
 
-$scope.clickedlist = function () {
-  $scope.$storage.activated = InboxService.storagearray();
-};
+  $scope.singleStar = function () {
+    InboxService.singleStar(this.message);
+  };
 
-$scope.nonclickedlist = function () {
-  $scope.$storage.activated = InboxService.nonstoragearray();
-  $scope.anySelect();
-};
+  $scope.icon = function () {
+    $scope.clicked=true;
+    // InboxService.iconButton();
+  };
+  //
+  $scope.noicon = function () {
+    $scope.clicked=false;
+    // InboxService.noIconButton();
+  };
 
-$scope.submitindex = function (num) {
-  var value = $scope.$storage.activated[num];
-  if (value === true){
-    $scope.$storage.activated[num] = false;
-  }else{
-    $scope.$storage.activated[num] = true;
-  } console.log($scope.$storage.activated);
-};
-
-$scope.read = function () {
-  for (var i = 0; i < $scope.messages.length; i++) {
-    if($scope.$storage.activated[i]===true){
-      $scope.messages[i].read = true;
-      console.log($scope.messages[i]);
-    }
-  }
-$scope.readcount();
-
-};
-
-$scope.unread = function () {
-  for (var i = 0; i < $scope.messages.length; i++) {
+  $scope.anySelect = function () {
+    var test = [];
+    for (var i = 0; i < $scope.$storage.activated.length; i++) {
       if($scope.$storage.activated[i]===true){
-          $scope.messages[i].read = false;
+        test.push(true);
+      }
+    }
+    if(test.length>0){
+      $scope.anyselected = true;
+    }else{
+      $scope.anyselected = false;
+    }
+  };
 
-  }
-} $scope.readcount();
+  $scope.readcount = function () {
+    var unreadarray = [];
+    for (var i = 0; i < $scope.messages.length; i++) {
+      if($scope.messages[i].read === false){
+        unreadarray.push($scope.messages[i]);
+      }
+    }
+    console.log(unreadarray);
+    $scope.unreadcount= unreadarray.length;
+  };
 
-};
 
-$scope.delete = function () {
-  for (var i = 0; i < $scope.messages.length; i++) {
+
+  $scope.clickedlist = function () {
+    $scope.$storage.activated = InboxService.storagearray();
+  };
+
+  $scope.nonclickedlist = function () {
+    $scope.$storage.activated = InboxService.nonstoragearray();
+    $scope.anySelect();
+  };
+
+  $scope.submitindex = function (num) {
+    var value = $scope.$storage.activated[num];
+    if (value === true){
+      $scope.$storage.activated[num] = false;
+    }else{
+      $scope.$storage.activated[num] = true;
+    } console.log($scope.$storage.activated);
+  };
+
+  $scope.read = function () {
+    for (var i = 0; i < $scope.messages.length; i++) {
       if($scope.$storage.activated[i]===true){
-          $scope.messages.splice(i,1);
-          $scope.$storage.activated.splice(i,1);
-  }
-} $scope.readcount();
-  $scope.anySelect();
-};
+        $scope.messages[i].read = true;
+        console.log($scope.messages[i]);
+      }
+    }
+    $scope.readcount();
 
-$scope.addlabel = function (filter) {
-  for (var i = 0; i < $scope.messages.length; i++) {
+  };
+
+  $scope.unread = function () {
+    for (var i = 0; i < $scope.messages.length; i++) {
+      if($scope.$storage.activated[i]===true){
+        $scope.messages[i].read = false;
+
+      }
+    } $scope.readcount();
+
+  };
+
+  $scope.delete = function () {
+    for (var i = 0; i < $scope.messages.length; i++) {
+      if($scope.$storage.activated[i]===true){
+        $scope.messages.splice(i,1);
+        $scope.$storage.activated.splice(i,1);
+      }
+    } $scope.readcount();
+    $scope.anySelect();
+  };
+
+  $scope.create = function (filter, i) {
+    if($scope.$storage.activated[i]===true){
+    if(filter === "createnew")
+    $scope.showcreate = true;
+  }
+  };
+
+  $scope.addlabel = function (filter) {
+    for (var i = 0; i < $scope.messages.length; i++) {
       if($scope.$storage.activated[i]===true){
         if(filter != "applyfilter")
-          if($scope.messages[i].filters.indexOf(filter)===-1)
-          $scope.messages[i].filters.push(filter);
-          console.log($scope.messages[i]);
-  }
-}
+          if(filter != "createnew")
+          if($scope.messages[i].filters.indexOf(filter)===-1){
+            $scope.messages[i].filters.push(filter);
+          }
+        } $scope.create(filter, i);
+    }
 
-};
+  };
+
+  
+
+
 
 
 
