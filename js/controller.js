1a58463eb584dd8172e76e264ef923bdd7d914b2
Amanda Carlton <acarlton27@gmail.com>
2015-09-29 09:30:28 -0600

    passing object to ss

--- js/controller.js
+++ js/controller.js
@@ -319,7 +319,11 @@ app.controller("InboxController", ['$scope', 'InboxService', '$location', '$loca
 }]);
 
 
-app.controller('ModalController', function($scope, close) {
+app.controller('ModalController', function($scope, close, $http) {
+  $scope.ok = function (newMessage) {
+    console.log(newMessage);
+   $http.post('http://localhost:3000/api/insert', {newMessage:newMessage});
+  };
 
  $scope.close = function(result) {
  	close(result); // close, but give 500ms for bootstrap to animate
